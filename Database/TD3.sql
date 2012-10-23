9e3d259b71822384433438cb6137acadc4ad0823
rewiko <anthony.comtois@gmail.com>
2012-10-23 17:07:22 +0300

    Update Database/TD3.sql

--- Database/TD3.sql
+++ Database/TD3.sql
@@ -1,8 +1,23 @@
+DROP TABLE ACCA;
+DROP TABLE FDC ;
+
 CREATE TABLE FDC(
-	fdcDept int not null,
+	fdcDept number not null,
 	fdcNumRue varchar(50),
 	fdcRue varchar(50),
-	fdcCP int(5),
+	fdcCP number(5),
 	fdcVille varchar(20),
-	primary key fdcDept 
-)	
+	primary key (fdcDept) ,
+	check (fdcDept<93)
+);
+
+CREATE TABLE ACCA(
+	accaCode number not null,
+	accaCommune varchar(50),
+	accaMontant varchar(50),
+	accaCP number(5),
+	accaVille varchar(20),
+	fk_fcdDept number not null,
+	primary key (accaCode), 
+	constraint fk_FDC foreign key  (fk_fcdDept) references FDC(fdcDept) 
+);
