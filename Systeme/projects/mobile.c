b31c61564ca173d949dde7ece5cc5784150cdbcb
rewiko <anthony.comtois@gmail.com>
2012-11-21 11:43:51 +0100

    mise en place structure procject

---
+++ Systeme/projects/mobile.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void aller_retour();
+void trajet_simple(char* point_A, char* point_B);
+
+int main(int argc,char* argv[],char* arge[]){
+ printf("%s\n",argv[0]);
+ 
+ return 0;
+}
+
+void aller_retour(char* point_A, char* point_B){
+
+
+
+}
+void trajet_simple(char* point_A, char* point_B){
+
+
+
+}
+
---
+++ Systeme/projects/vehicule.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char* argv[],char* arge[]){
+ printf("%s\n",argv[0]);
+ 
+ return 0;
+}
